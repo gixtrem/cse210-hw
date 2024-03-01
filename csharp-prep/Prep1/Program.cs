304573120f11cfafe96061282f8b61e19182a909
Godday Umah <god21010@byui.edu>
2024-02-29 18:01:20 -0700

    new project

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+    Console.Write("What is your first name? ");
+    string firstname = Console.ReadLine();
+    Console.Write("Whats your last name? ");
+    string lastname = Console.ReadLine();
+    Console.WriteLine($"Your name is, {firstname} {lastname}.");
+
     }
 }
